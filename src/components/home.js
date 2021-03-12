89bd55f83cc440d5cb2508172565a2934626dc4b
Mike Bird <mikebird148@gmail.com>
2021-03-11 21:39:56 -0800

    finalized functionality of Daily Smarty searching

---
+++ src/components/home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Logo from './logo';
+import SearchBar from './searchBar';
+import RecentPosts from './recentPosts';
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Home extends Component {
+  handleSearchBarSubmit(query) {
+    this.props.fetchPostsWithQuery(query);
+    this.props.history.push('/results');
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <Logo />
+          <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
+          <RecentPosts />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(Home);
---
+++ src/components/searchBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+import { withRouter } from 'react-router-dom';
+
+class SearchBar extends Component {
+
+    handleFormSubmit = function({query}) {
+        this.props.onSubmit(query);
+    }
+    
+    renderInput(field) {
+        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+    }
+
+    render() {
+
+        const { handleSubmit } = this.props;
+
+        return (
+            <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <Field name="query" component={this.renderInput} />
+            </form>
+        )
+    }
+
+}
+
+SearchBar = reduxForm({
+    form: 'searchBar'
+})(SearchBar);
+
+SearchBar = withRouter(SearchBar);
+
+export default SearchBar;
